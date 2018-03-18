d8688af996c40b367baa0254dd3d65f536b28ea0
Matheus Lima <matheusml90@gmail.com>
2018-03-18 15:25:46 -0300

    Adding body-parser and cors

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -1,9 +1,14 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const getRandomMessage = require("./services/randomMessages").getRandomMessage;
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/messages", (_, response) => {
   response.send(getRandomMessage());
