6ba94e101846dfe4508769e72506a75a5d5f3680
Matheus Lima <matheusml90@gmail.com>
2018-03-31 17:11:29 -0300

    Fixing timeout react native warnings

--- front-end/App.js
+++ front-end/App.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import Routes from "./src/components/routes/Routes";
 
+/*
+  Setting timers for multiple minutes isn't handled properly in React Native on Android.
+  The line above "solves" the problem. 
+  More info here: https://github.com/facebook/react-native/issues/12981
+*/
+console.ignoredYellowBox = ["Setting a timer"];
+
 export default class App extends React.Component {
   render() {
     return <Routes />;
